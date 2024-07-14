edec5812d1861cab210d1c3fa4e9060249589df4
Chaisuu <Kfire13@yandex.ru>
2024-07-14 22:19:41 +0700

    add in widget.py masks for card new variable new_card_

--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,22 @@
-def get_mask_card_number(card_number: int) -> int:
+def get_mask_card_number(number: int) -> int:
     """Функция которая возвращает маску карты"""
-    return card_number[:4] + " " + card_number[4:6] + "** ****" + card_number[-4:]
+    return number[:4] + " " + number[4:6] + "** ****" + number[-4:]
 
 
-def get_mask_account(card_number: int) -> int:
+def get_mask_account(number: int) -> int:
     """Функция которая маскирует номер счета"""
-    return "**" + card_number[-4:]
+    return "**" + number[-4:]
 
 def mask_account_card(numbers: str) -> str:
     if len(numbers.split()[1]) == 16:
+        new_card = get_mask_card_number(numbers.split()[1])
         result = f'{numbers[:-16]}{get_mask_card_number(numbers.split()[1])}'
 
+
+    elif len(numbers.split()[1]) == 20:
+        result = f'{numbers[:-20]}'{get_mask_account(number.split()[1])}
+
+    return result
 
     #name_card = numbers[:-16]
     #account_ = numbers.split()[1]
