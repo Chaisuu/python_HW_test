db13e0b9787666d576d93be74eee931f3f2a5e11
Chaisuu <Kfire13@yandex.ru>
2024-07-14 22:03:18 +0700

    add in widget.py masks for card

--- src/widget.py
+++ src/widget.py
@@ -7,9 +7,19 @@ def get_mask_account(card_number: int) -> int:
     """Функция которая маскирует номер счета"""
     return "**" + card_number[-4:]
 
-def mask_account_card(number:str) -> str
-    account_ = mask_account_card.split(' ')[1]
-    print(account_)
+def mask_account_card(numbers: str) -> str:
+    if len(numbers.split()[1]) == 16:
+        result = f'{numbers[:-16]}{get_mask_card_number(numbers.split()[1])}'
+
+
+    #name_card = numbers[:-16]
+    #account_ = numbers.split()[1]
+    #new_masks_card = get_mask_card_number(numbers.split()[1])
+    #print(account_)
+    #print(name_card)
+    #print(new_masks_card)
+
+
 
 
 mask_account_card('Maestro 1596837868705199')
