67a6fe888e2059148d8490801be7283e624c03ea
Chaisuu <Kfire13@yandex.ru>
2025-01-04 00:06:53 +0700

    add logs for utils1.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,23 @@
+import logging
+
+logger = logging.getLogger('__name__')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('../logs/masks.log', encoding="utf-8", mode="w")
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: str | int) -> str:
     card_number = str(card_number)
     """Функция которая возвращает маску карты"""
+    logger.info("Mask for card numer created")
     return card_number[:4] + " " + card_number[4:6] + "** ****" + " " + card_number[-4:]
 
 
 def get_mask_account(card_number: str | int) -> str:
     card_number = str(card_number)
     """Функция которая маскирует номер счета"""
+    logger.info("Mask for account created")
     return "**" + " " + card_number[-4:]
--- src/utils1.py
+++ src/utils1.py
@@ -1,51 +1,56 @@
 import json
 import os
-
 from json import JSONDecodeError
 from typing import Any
+import logging
 from src.external_api import get_amount_convert
+
+#if not os.path.exists('logs'):
+#    os.makedirs('logs')
+
+logger = logging.getLogger('__name__')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('../logs/utils1.log', encoding="utf-8", mode="w")
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def get_read_json(path: str) -> list[Any] | Any:
     """Функция которя принимает путь на вход и возрващает список словарей с данными о транзакциях"""
 
     try:
-        """Eсли файл пустой, содержит не список или не найден, функция возвращает пустой список"""
         with open(path, 'r', encoding="utf-8") as f:
+            logger.info("pass path where file is located")
             try:
                 transactions = json.load(f)
+                logger.info("open file")
             except JSONDecodeError:
+                logger.error("Error JSONDecode")
                 return []
             else:
                 return transactions
     except FileNotFoundError:
+        logger.error("Error: type file not lisr or FileNotFound")
         return []
     else:
         return transactions
 
 
-def get_amount_transactions(transac: list) -> Any:
+def get_amount_transactions(transaction: list) -> Any:
     """Функция которая принимает на вход транзакцию и возращает сумму в рублях """
     currency = 'RUB'
-    if transac["operationAmount"]["currency"]["code"] == currency:
-        amount = transac["operationAmount"]["amount"]
+    logger.info("amount in RUB")
+    if transaction["operationAmount"]["currency"]["code"] == currency:
+        amount = transaction["operationAmount"]["amount"]
     else:
-        amount = get_amount_convert(transac)
+        logger.info("amount in RUB after conversion")
+        amount = get_amount_convert(transaction)
+
     return amount
 
 
-#if __name__ == '__main__':
-#    PATH_TO_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
-#    print(get_read_json(PATH_TO_FILE))
-
-#transac = {
-#    'id': 879660146, 'state': 'EXECUTED', 'date': '2018-07-22T07:42:32.953324',
-#    'operationAmount': {'amount': '92130.50', 'currency': {'name': 'USD', 'code': 'USD'}},
-#    'description': 'Перевод организации', 'from': 'Счет 19628854383215954147',
-#    'to': 'Счет 90887717138446397473'}
-#print(get_amount_transactions(transac))
-
-#    transac = {'id': 441945886, 'state': 'EXECUTED', 'date': '2019-08-26T10:50:58.294041',
-#                 'operationAmount': {'amount': '31957.58', 'currency': {'name': 'руб.', 'code': 'RUB'}},
-#                 'description': 'Перевод организации', 'from': 'Maestro 1596837868705199',
-#                 'to': 'Счет 64686473678894779589'}
+if __name__ == '__main__':
+    PATH_TO_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "operations.json")
+    print(get_read_json(PATH_TO_FILE))
